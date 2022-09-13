cec4368b7f73d80962a068ac265665a2d5376f93
rijomp <rijomp99@gmail.com>
2022-09-13 22:32:00 +0530

    test

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(function(err, req, res, next) {
   })
 });
 
-app.listen(process.env.PORT || 5000, ()=>{console.log("server statrted at 5000")})
+app.listen(process.env.PORT || 4675, ()=>{console.log("server statrted at 5000")})
 
 
 module.exports = app;
